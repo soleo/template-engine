70841f6e9872968bd6d7150b4fd18c68fca6d1c4
Xinjiang Shao <shaoxinjiang@gmail.com>
2023-01-19 19:44:06 -0600

    first commit

---
+++ src/main/kotlin/com/xinjiangshao/templateengine/TemplateEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.xinjiangshao.templateengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemplateEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemplateEngineApplication>(*args)
+}
---
+++ src/test/kotlin/com/xinjiangshao/templateengine/TemplateEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xinjiangshao.templateengine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
